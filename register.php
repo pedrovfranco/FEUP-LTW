48a1913fba91f991fdb0e3ca137a5125a56e7d4e
pedrofixe <yurbypedro@hotmai.com>
2018-10-28 18:01:33 +0000

    Added stuff

--- register.php
+++ register.php
@@ -13,42 +13,43 @@
 	$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
 
 	$query1 = $dbh->prepare('SELECT * FROM User WHERE username = ?');
-	$query1->execute(array($username);
+	$query1->execute(array($username));
 
-	if (count($query1) > 0)
+	if (count($query1->fetchAll()) > 0)
 	{
 		echo "Username already taken!<br>";
-		sleep(3);
-		header("Location: https://web.fe.up.pt/~up201604828/projeto/register.html");
-		exit(1);
-	}
-
-	$query2 = $dbh->prepare('SELECT * FROM User WHERE email = ?');
-	$query2->execute(array($email));
-
-	if (count($query2->fetchAll()) > 0)
-	{
-		echo "Email already taken!<br>";
-		sleep(3);
-		header("Location: https://web.fe.up.pt/~up201604828/projeto/register.html");
-		exit(1);
-	}
-
-	$register = $dbh->prepare('INSERT INTO User VALUES (NULL, ?, ?, ?, ?)');
-	
-	$dbh->beginTransaction();
-
-	$register->execute(array($username, $hash, $age, $email));
-
-	$dbh->commit();
-
-	if (!$register->execute(array($username, $hash, $age, $email)))
-	{
-	    echo "\nPDO::errorInfo():\n";
-	    print_r($dbh->errorInfo());
-
-	    echo "Error on insert!<br>";
 	}
 	else
-		displayUsers($dbh);
+	{
+		$query2 = $dbh->prepare('SELECT * FROM User WHERE email = ?');
+		$query2->execute(array($email));
+
+		if (count($query2->fetchAll()) > 0)
+		{
+			echo "Email already taken!<br>";
+		}
+		else
+		{
+			$register = $dbh->prepare('INSERT INTO User VALUES (NULL, ?, ?, ?, ?)');
+	
+			$dbh->beginTransaction();
+
+			$status = $register->execute(array($username, $hash, $age, $email));
+
+			$dbh->commit();
+
+			if (!$status)
+			{
+			    echo "\nPDO::errorInfo():\n";
+			    print_r($dbh->errorInfo());
+
+			    echo "Error on insert!<br>";
+			}
+			else
+				echo "Register sucessful!<br>";
+		}
+	}
+
+	
+	echo "<br><a href=\"register.html\">Go Back</a> </li>";
 ?>
