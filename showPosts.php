acc5335408f90baa1835a8143b713b2dd377357b
pedrofixe <yurbypedro@hotmai.com>
2018-10-29 22:40:15 +0000

    php refuses to work

---
+++ showPosts.php
@@ -0,0 +1,33 @@
+<?php
+
+	ini_set('display_errors', 1);
+
+	$dbh = new PDO('sqlite:database.db');
+
+	$query = $dbh->prepare('SELECT * from Post');
+
+	$status = $query->execute();
+
+	if (!$status)
+	{
+	    echo "\nPDO::errorInfo():\n";
+	    print_r($dbh->errorInfo());
+
+	    echo "Error!<br>";
+	}
+
+	while ($row = $query->fetch())
+	{
+		$a = $row['idPost'];
+		$b = $row['idUser'];
+		$c = $row['Title'];
+		$d = $row['Text'];
+		$e = $row['Link'];
+		$f = $row['Date'];
+		$g = $row['Upvotes'];
+		$h = $row['Downvotes'];
+
+		echo "$a | $b | $c | $d | $e | $f | $g | $h<br>";
+	}
+
+?>
